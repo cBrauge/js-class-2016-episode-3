d8243b0e8003eca22250ad37441ed5af45c9f9ad
Corentin Brauge <brauge_c@epita.fr>
2016-03-29 11:51:43 +0200

    ex02

--- src/client/exercise-02/ng/components/layout.js
+++ src/client/exercise-02/ng/components/layout.js
@@ -3,9 +3,23 @@ import 'client/exercise-02/ng/services/chuck-norris';
 window._app.global_ng_module
 .component('layout', {
   templateUrl: 'client/exercise-02/ng/components/layout.html',
-  controller: ['chuckNorris', function(chuckNorris) {
+  controller: ['chuckNorris', '$scope', function(chuckNorris, $scope) {
     chuckNorris.fetch3Random().then((res) => {
       console.log(res.data.value);
+      $scope.facts = res.data.value;
     })
   }]
+});
+
+window._app.global_ng_module
+.component('fact', {
+  templateUrl: 'client/exercise-02/ng/components/facts.html',
+  controller: ['chuckNorris', '$scope', function($scope) {
+    console.log(this.facts);
+    $scope.facts = this.facts;
+    console.log($scope.facts);
+  }],
+  bindings: {
+    facts: '='
+  }
 });
